a37c44fc572faf1d72d8b6db6bba5632cdc6c1ec
Yegor Bugayenko <yegor256@gmail.com>
2024-09-10 22:23:34 +0300

    entry

---
+++ entry.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# MIT License
+#
+# Copyright (c) 2024 Zerocracy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -ex
+set -o pipefail
+
+id=$1
+if [ -z "${id}" ]; then
+  echo "The first argument must be the ID of the job to process"
+  exit 1
+fi
+
+home=$2
+if [ -z "${home}" ]; then
+  echo "The second argument must be the directory where 'base.fb' is located"
+  exit 1
+fi
+
+self=$(dirname "$0")
+
+judges --verbose update --quiet --summary --max-cycles=3 --no-log --lib "${self}/lib" "${self}/judges" "${home}/base.fb"
