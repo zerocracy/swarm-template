dac89f4f8dd0766107bb454db822a9268b472466
VasilevNStas <ctac@outlook.com>
2026-06-08 21:29:47 +0300

    #258: Remove duplicate Author/Copyright comments from test_say.rb

--- test/test_say.rb
+++ test/test_say.rb
@@ -7,10 +7,6 @@ require 'minitest/autorun'
 require_relative 'test__helper'
 require_relative '../lib/say'
 
-# Test for "say.rb".
-# Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2024 Yegor Bugayenko
-# License:: MIT
 class TestSay < Minitest::Test
   def test_simple_scenario
     out = say('Hello, {name}!')
